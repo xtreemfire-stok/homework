9950b0312dc06a3d9cad3bde281add6524778ea6
xtreemfire-stok <arhipovio@mail.ru>
2024-11-19 00:24:27 +0300

    Решение задачи

--- main_two.py
+++ main_two.py
@@ -1,9 +1,11 @@
-"""Домашнее задание по второму семинару"""
+"""Домашнее задание по третьему семинару"""
 
 """Задача №1"""
 
 n = int(input())
-i = n
-min = i
-
-for i in range(n)
+k = 0
+for i in range(n):
+    v = int(input())
+    if v == 1:
+        k += 1
+print(k if k < n / 2 else n - k)
