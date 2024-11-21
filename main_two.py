2fb42d3049b9e66b219534f58d0eeb82a41448c6
xtreemfire-stok <arhipovio@mail.ru>
2024-11-21 15:45:00 +0300

    21.11.2024

--- main_two.py
+++ main_two.py
@@ -44,18 +44,47 @@
 
 """Задача №2"""
 
-n = int(input())
-list_1 = list()
-for i in range(n):
-    x = int(input())
-    list_1.append(x)
+# n = int(input())
+# list_1 = list()
+# for i in range(n):
+#     x = int(input())
+#     list_1.append(x)
 
-k = int(input())
-m = abs(k - list_1[0]) 
+# k = int(input())
+# m = abs(k - list_1[0]) 
 
-number = list_1[0]
-for i in range(1, len(list_1)):
-    if m > abs(list_1[i] - k):
-        m = abs(list_1[i] - k)
-        number = list_1[i]
-print(number)
+# number = list_1[0]
+# for i in range(1, len(list_1)):
+#     if m > abs(list_1[i] - k):
+#         m = abs(list_1[i] - k)
+#         number = list_1[i]
+# print(number)
+
+
+"""Здача #3"""
+
+dict_with_letter = {1: ("А","Е","В","И","Н","О","Р","С","Т", 
+ 'A','E','I','O','U','L','N','S','T','R'), 
+ 2: ("Д","К","Л","М","П","У", 
+ 'D', 'G'), 
+ 3: ("Б","Г","Ё","Ь","Я", 
+ 'B','C','M', 'P'), 
+ 4: ("Й","Ы", 
+ 'F', 'H', 'V', 'W', 'Y'), 
+ 5: ("Ж","З","Х","Ц","Ч", 
+ 'K'), 
+ 6: ("Ш", "Э","Ю", 
+ 'J','X'), 
+ 7: ("Ф","Щ","Ъ", 
+ 'Q','Z') 
+ } 
+ 
+def dicting(dict_with, words): 
+ sum_points = 0 
+ for key, value in dict_with.items(): 
+  for letter in words.upper(): 
+   if letter in value: 
+    sum_points += key 
+ print(sum_points) 
+word = str(input()) 
+dicting(dict_with_letter,word)
