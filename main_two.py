16cb7d105accf82db2a63602a21e85f42938b42d
xtreemfire-stok <arhipovio@mail.ru>
2024-12-08 11:38:20 +0300

    08.12.2024

--- main_two.py
+++ main_two.py
@@ -133,13 +133,26 @@
 """Задача №2"""
 
 
-def sum(a, b):
-    if b == 0:
-        return a
-    else:
-        if b > 0:
-            return sum(a + 1, b - 1)
-        else:
-            return sum(a - 1, b + 1)
+# def sum(a, b):
+#     if b == 0:
+#         return a
+#     else:
+#         if b > 0:
+#             return sum(a + 1, b - 1)
+#         else:
+#             return sum(a - 1, b + 1)
 
-print(sum(int(input()), int(input())))
+# print(sum(int(input()), int(input())))
+
+
+"""Домашнее задание по пятому семинару"""
+
+
+"""Задача #1"""
+
+
+a1 = int(input())
+d = int(input())
+n = int(input())
+for i in range(n):
+    print(a1 + i * d, end=' ')
