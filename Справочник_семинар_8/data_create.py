eae4ddb79fd6a57b0f7815e8b71715dfdbd0415f
xtreemfire-stok <arhipovio@mail.ru>
2024-12-17 01:06:58 +0300

    17.12.2024

---
+++ Справочник_семинар_8/data_create.py
@@ -0,0 +1,16 @@
+def name_data():
+    name = int(input("Введите ваше имя: "))
+    print("Очень красивое имя")
+    return name
+
+def surname_data():
+    surname = int(input("Введите вашу фамилию: "))
+    return surname
+
+def phone_data():
+    phone = int(input("Введите ваш телефон: "))
+    return phone
+
+def address_data():
+    address = int(input("Введите ваш адрес: "))
+    return address
